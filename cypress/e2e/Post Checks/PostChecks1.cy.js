e573bb803df662e4441129792a35431a54f8d220
pruthvirajqa2dev <pruthvirajqa2dev>
2024-08-20 17:56:36 +0530

    Postchecks-20/08

--- cypress/e2e/Post Checks/PostChecks1.cy.js
+++ cypress/e2e/Post Checks/PostChecks1.cy.js
@@ -120,11 +120,11 @@ describe('Postchecks TC9 onwards', () => {
         cy.log("Net Invoice: " + netInvoice)
         const vatAmount = Math.round((netInvoice * vatPercent / 100) * 100) / 100
         cy.log("VAT Amount: " + vatAmount)
-    
-        const totalInvoiceValue = Math.round((netInvoice + vatAmount )* 100) / 100
+
+        const totalInvoiceValue = Math.round((netInvoice + vatAmount) * 100) / 100
         cy.log("Total Invoice: " + totalInvoiceValue)
         //############Invoice calculation END #################################
-    
+
         cy.log("Adding invoice for Supplier:" + testData.supplierName)
         //Click Hamburger
         cy.log("Click on Hamburger")
@@ -134,28 +134,28 @@ describe('Postchecks TC9 onwards', () => {
         cy.log("Type screen in the text box")
         cy.get('.quick-lunch').eq(1)
           .find('#esr_launch_text').clear().type(`${screen}`)
-    
+
         //Click on the menu item displayed
         cy.log("Click on the menu item displayed")
         cy.get('.ui-menu-item')
           .contains(screen).click()
-    
+
         //Select school      
         cy.selectUsingSearchIcon('company_id', testData.schoolId)
-    
+
         cy.log("************PRL300Q screen**************")
         cy.log("Click on search button")
-    
+
         cy.get('#search_button')
           .click()
-    
+
         //New Button
         cy.log("Click in new button(with dropdown)")
         cy.get('.multibutton_content')
           .find('.esr_multibutton:contains("New")')
           .eq(0)
           .click()
-    
+
         if (testData.invoiceType == 'Non Purchase Order Invoice') {
           cy.get('#invoice_type2')
             .check()
@@ -166,13 +166,13 @@ describe('Postchecks TC9 onwards', () => {
             .check()
             .should('be.checked')
         }
-    
+
         cy.get('#select_button')
           .click()
-    
+
         cy.get('.esr_breadcrumb_selected')
           .contains('Non Purchase Order Details')
-    
+
         cy.get('#supplier_icon')
           .click()
         cy.get('[axes="SUPP_NAME"]')
@@ -181,87 +181,88 @@ describe('Postchecks TC9 onwards', () => {
           .parent()
           .contains('Select')
           .click()
-    
+
         cy.get('[for="supplier"]')
           .eq(1)
           .should('have.text', testData.supplierName)
         cy.get('#doc_date')
           .type(testData.invoiceDate)
-    
+
         cy.get('[for="authority_code"]').eq(0).click()
-    
+
         cy.get('.ui-pnotify-text')
           .should('contain', testData.period)
         cy.get('#tot_value')
           .type(totalInvoiceValue)
         cy.get('#tot_vat').type(vatAmount)
-    
+
         //Add unique invoice refernce
         const timestamp = Date.now()
         const supplierInvoiceRef = 'INVChequeRUN' + timestamp
+        cy.wrap(supplierInvoiceRef).as("invoiceRef" + i)
         cy.get('#supp_own_ref')
           .type(supplierInvoiceRef)
-    
+
         //***Add line 
         cy.get('.multibutton_content > .esr_hover')
           .contains('Add')
           .click()
-    
+
         //Dialog title verification
         cy.get('.ui-dialog-title')
           .should('contain', 'Invoice/Credit Note')
-    
+
         cy.get('#narr_desc')
           .type('Test Description')
-    
+
         //GL Code
         cy.selectCostCentre(costCentre)
         cy.selectFirstLedgerCodeAndFundCode()
-    
+
         cy.get('#line_quantity').type(quantity)
         cy.get('#unit_price').type(unitprice)
         cy.get('*[id^=ui-id]').contains("Invoice/Credit Note").click()
         cy.get('#vat_exclusive').invoke('val')
-    
+
         cy.get('#vat_code_icon')
           .click()
-    
+
         cy.get('[axes="VACODE"] > div')
           .contains(vatCode)
           .parent()
           .parent()
           .find('#esr_action')
           .click()
-    
+
         cy.get('*[id^=ui-id]').contains("Invoice/Credit Note").click()
-        cy.get('#vat_value').invoke('val').should('eq', vatAmount!=0?vatAmount.toFixed(2).toLocaleString():"0.00")
-        cy.get('#total_line_value').invoke('val').should('eq',totalInvoiceValue.toFixed(2).toString())
-    
+        cy.get('#vat_value').invoke('val').should('eq', vatAmount != 0 ? vatAmount.toFixed(2).toLocaleString() : "0.00")
+        cy.get('#total_line_value').invoke('val').should('eq', totalInvoiceValue.toFixed(2).toString())
+
         cy.get('[data-originalvalue="Save"]').click()
         //Assert
-        cy.get('[axes="LINE_QUANTITY"] > div').invoke('text').should('contain',quantity)
-        cy.get('[axes="UNIT_PRICE"] > div').invoke('text').should('contain',unitprice)
-        cy.get('[axes="VAT_EXCLUSIVE"] > div').invoke('text').should('contain',String(netInvoice.toLocaleString()))
-        cy.get('[axes="VAT_CODE"] > div').invoke('text').should('contain',vatCode)
-        cy.get('[axes="VAT_VALUE"]').invoke('text').should('contain',String(vatAmount.toFixed(2)))
-    
+        cy.get('[axes="LINE_QUANTITY"] > div').invoke('text').should('contain', quantity)
+        cy.get('[axes="UNIT_PRICE"] > div').invoke('text').should('contain', unitprice)
+        cy.get('[axes="VAT_EXCLUSIVE"] > div').invoke('text').should('contain', String(netInvoice.toLocaleString()))
+        cy.get('[axes="VAT_CODE"] > div').invoke('text').should('contain', vatCode)
+        cy.get('[axes="VAT_VALUE"]').invoke('text').should('contain', String(vatAmount.toFixed(2)))
+
         cy.get('[data-originalvalue="Finish & Save"]').click()
-        
+
         cy.get('*[id*=summary_details]').should('be.visible')
         //Assert
-        cy.get('#tot_value').invoke('text').should('eq',String(totalInvoiceValue.toLocaleString()))
-        cy.get('#tot_vat').invoke('text').should('contain',String(vatAmount.toLocaleString()))
-        cy.get('#company_id').invoke('text').should('eq',testData.schoolId)
+        cy.get('#tot_value').invoke('text').should('eq', String(totalInvoiceValue.toLocaleString()))
+        cy.get('#tot_vat').invoke('text').should('contain', String(vatAmount.toLocaleString()))
+        cy.get('#company_id').invoke('text').should('eq', testData.schoolId)
         cy.get('#ok_button').click()
       }
     })
   })
-  it.only('test', () => {
+  it('test', () => {
     let quantArr = [1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1]
-    const quantity =1// String(quantArr[Math.floor(Math.random() * quantArr.length)])
+    const quantity = 1// String(quantArr[Math.floor(Math.random() * quantArr.length)])
     cy.log("Quantity: " + quantity)
     let unitPriceArr = [120.12, 230.23, 340.34, 450.45, 560.56, 670.67, 780.78]
-    const unitprice =111.10// String(unitPriceArr[Math.floor(Math.random() * unitPriceArr.length)])
+    const unitprice = 111.10// String(unitPriceArr[Math.floor(Math.random() * unitPriceArr.length)])
     cy.log("Unit price: " + unitprice.toFixed(2))
     let vatCodeArr = ["EXM", "NON", "RED", "STD", "ZER"]
     const vatCode = "RED"// vatCodeArr[Math.floor(Math.random() * vatCodeArr.length)]
@@ -289,8 +290,104 @@ describe('Postchecks TC9 onwards', () => {
     const vatAmount = Math.round((netInvoice * vatPercent / 100) * 100) / 100
     cy.log("VAT Amount: " + vatAmount.toFixed(2))
 
-    const totalInvoiceValue = Math.round((netInvoice + vatAmount )* 100) / 100
+    const totalInvoiceValue = Math.round((netInvoice + vatAmount) * 100) / 100
     cy.log("Total Invoice: " + totalInvoiceValue.toFixed(2))
+
+  })
+  it.only('PRL610Q - Cheque Processing', () => {
+    const username = testData.username
+    const password = testData.password
+    const screen = testData.PRL610Q
+
+    cy.login(username, password)
+    //Click Hamburger
+    cy.log("Click on Hamburger")
+    cy.get('#banner_navigation_navigate')
+      .should('be.visible').click()
+
+    //Click on the text box in Quick launch
+    cy.log("Type screen in the text box")
+    cy.get('.quick-lunch').eq(1)
+      .find('#esr_launch_text').clear().type(`${screen}`)
+
+    //Click on the menu item displayed
+    cy.log("Click on the menu item displayed")
+    cy.get('.ui-menu-item')
+      .contains(screen).click()
+
+    cy.log("Click on new button")
+    cy.get('.multibutton_content')
+      .find('.esr_multibutton:contains("New")')
+      .eq(0)
+      .click()
+    cy.log("Select School Id")
+    cy.selectUsingSearchIcon('company_id', testData.schoolId)
+
+    cy.get('#run_narr').type('TestNarrChqRun')
+    cy.get('#refresh').click()
+
+    cy.get('*[id^=ui-id]').contains("Please wait...").should('be.visible')
+    cy.get('#processing_controls').contains("Pending").should('be.visible')
+    cy.get('[axes="SUPP_NAME"] > div', { timeout: 150000 }).should('be.visible')
+
+    //Assert invoice reference
+    cy.get('[axes="SUPP_REF"] > div').invoke('text')
+      .should('contain', 'INVChequeRUN'.toUpperCase())
+
+    cy.log("Check all invoices")
+    cy.get('*[id^=esr_grid_column_row_check_all]').check()
+
+    cy.log("Click next")
+    cy.get('#next_button').click()
+
+    cy.log("Dialog box for negative ")
+    cy.get('*[id^=ui-id]').contains("Transaction Selection").should('be.visible')
+
+    cy.get('*[id$=PRL610Q0_esr_prompt] > div').invoke('text')
+      .should('contain', 'New Balance will be negative. Do you want to continue?')
+
+    cy.get('#esr_messagebox_yes').click()
+
+    cy.log("Dialog box for payment run")
+    cy.get('*[id^=ui-id]').contains("Transaction Selection").should('be.visible')
+
+    cy.get('*[id$=PRL610Q1_esr_prompt] > div').invoke('text')
+      .should('contain', 'Payment run must be confirmed on same date as entered.')
+
+    cy.get('#esr_messagebox_ok').click()
+
+    cy.get('#INV_CRN_STEP').should('be.visible')
+
+    cy.get('.multibutton_content')
+      .find('.esr_multibutton:contains("Add All")')
+      .eq(0)
+      .click()
+
+    cy.get('.multibutton_content')
+      .find('.esr_multibutton:contains("Remove All")')
+
+    cy.get('*[id*=chequeprocessingpage2_esr_cn]').contains('Cheque Books to be used in the order listed below')
+    cy.get('[axes="START_NO"] > div').should('be.visible')
+
+    cy.get('#next_button').click()
+
+    cy.get('#STEP_3').should('be.visible')
+
+    //Assert Supplier name
+    cy.get('[axes="SUPP_NAME"] > div').invoke('text')
+      .should('contain', 'Eastern Water Authority')
+
+    cy.get('#print_report').click()
+    cy.get('*[id^=ui-id]').contains("Please wait...").should('be.visible')
+    cy.window().then((win) => {
+      const orig = win.open
+
+      win.open = function (url, target, features) {
+        return orig.call(this, url, '_self', features)
+      }
+    })
+    cy.get('#spc_rep_0', { timeout: 150000 }).click()
+
 
   })
 })
